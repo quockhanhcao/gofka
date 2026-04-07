466a58da84c1347ecd4cc830fc3c9d3cf48ccd9c
quockhanhcao99 <quockhanhcao99@gmail.com>
2026-04-08 00:02:39 +0700

    feat: Initial tcp server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gofka
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if os.Args[1] == "server" {
+		startServer()
+	} else {
+		clientConnect()
+	}
+}
+
+func startServer() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Printf("error creating server %v", err)
+		return
+	}
+	conn, err := ln.Accept()
+	if err != nil {
+		fmt.Printf("error accepting connection %v", err)
+		return
+	}
+	streamReadWrite := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	length, _ := streamReadWrite.ReadByte()
+	data, _ := streamReadWrite.Peek(int(length))
+	streamReadWrite.Discard(int(length))
+
+	fmt.Printf("received from client %s", string(data))
+
+	streamReadWrite.WriteByte(byte(len(data)))
+	streamReadWrite.WriteString(string(data))
+	streamReadWrite.Flush()
+
+	conn.Close()
+}
+
+func clientConnect() {
+	conn, err := net.Dial("tcp", ":8080")
+	if err != nil {
+		fmt.Printf("error %v", err)
+		return
+	}
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	fmt.Printf("sending to server %s", line)
+	// send to server
+	// a simple protocol: a message has to attach a byte for message length before the actual message
+	streamWriter := bufio.NewWriter(conn)
+	streamWriter.WriteByte(byte(len(line)))
+	streamWriter.WriteString(line)
+	streamWriter.Flush()
+
+	// var str []byte
+	// str = append(str, byte(len(line)))
+	// str = append(str, []byte(line)...)
+
+	// conn.Write(str)
+
+	streamReader := bufio.NewReader(conn)
+	length, _ := streamReader.ReadByte()
+	data, _ := streamReader.Peek(int(length))
+
+	fmt.Printf("received from server %s", string(data))
+	conn.Close()
+}
